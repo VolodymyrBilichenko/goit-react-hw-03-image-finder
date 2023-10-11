5620176bdfa4f18fbd285f566e98c6aa99ae552f
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-11 12:20:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,98 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Container } from './Container/Container';
+import { getAllImages } from 'Api/ImagesApi';
+import { GlobalStyle } from './GlobalStyle/GlobalStyled.styled';
+import { SearchBar } from './SearchBar/SearchBar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { NotFound } from './NotFound/NotFound';
+import { LoadMore } from './LoadMore/LoadMore';
+import { Loading } from './Loading/Loading';
+import { toast } from 'react-toastify';
+
+export class App extends Component {
+  state = {
+    images: [],
+    error: '',
+
+    page: 1,
+    perPage: 12,
+    searchQuery: '',
+
+    isLoading: false,
+    showNotFound: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page, searchQuery } = this.state;
+
+    if (prevState.page !== page || prevState.searchQuery !== searchQuery) {
+      this.handleImages(searchQuery, page);
+    }
+  }
+
+  handleImages = async () => {
+    const { page, perPage, searchQuery } = this.state;
+    try {
+      this.setState({ isLoading: true });
+      const data = await getAllImages(searchQuery, page, perPage);
+      this.setState({ isLoading: false });
+      console.log(data);
+      if (page === 1) {
+        this.setState({ images: data.hits });
+      } else {
+        this.setState(set => ({
+          images: [...set.images, ...data.hits],
+        }));
+      }
+
+      if (data.hits.length === 0) {
+        this.setState({ showNotFound: true });
+      } else {
+        this.setState({ showNotFound: false });
+      }
+    } catch (error) {
+      this.setState({ error: error.message, isLoading: false });
+    }
+  };
+
+  handleSearch = searchQuery => {
+    if (
+      this.state.searchQuery.toLowerCase().trim() ===
+      searchQuery.toLowerCase().trim()
+    ) {
+      return toast.warn(`You are already viewing ${searchQuery}`);
+    }
+
+    this.setState({
+      searchQuery: searchQuery.toLowerCase().trim(),
+      page: 1,
+      images: [],
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  render() {
+    const { images, showNotFound, isLoading } = this.state;
+    return (
+      <>
+        <GlobalStyle />
+        {isLoading && <Loading />}
+        <SearchBar onSubmit={this.handleSearch} />
+        <Container>
+          <ImageGallery imagesLi={images} />
+          {showNotFound && <NotFound />}
+
+          {images.length > 0 && (
+            <LoadMore
+              onClick={this.onLoadMore}
+              isVisible={!this.state.isLoading}
+            />
+          )}
+        </Container>
+      </>
+    );
+  }
+}
