9270aef2a8ead1e676b1a53557a9b7ad935a300f
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-25 15:54:39 +0300

    Update ImageGalleryPopUp.jsx

--- src/components/ImageGalleryPopUp/ImageGalleryPopUp.jsx
+++ src/components/ImageGalleryPopUp/ImageGalleryPopUp.jsx
@@ -8,7 +8,7 @@ export const ImageGalleryPopUp = ({ showModal, largeImageURL, onClose }) => {
     return (
         <ImageGalleryPopUpStyle className="popup">
             <div className="modal">
-                <img src={largeImageURL} alt="Large Image" />
+                <img src={largeImageURL} alt="Beautiful" />
                 <button className="popup__close" onClick={onClose}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 43 43" fill="none">
                     <line x1="10.8087" y1="10.4047" x2="32.0219" y2="31.6179" stroke="#C3A488" strokeWidth="4"/>
